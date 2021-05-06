dae078eea6c041a452a8520056f605bc69494956
Leonards03 <edwin.leonards03@gmail.com>
2021-05-06 17:11:55 +0700

    First Commit

---
+++ app/src/main/java/com/bangkit/capstone/idku/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.bangkit.capstone.idku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.capstone.idku.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
