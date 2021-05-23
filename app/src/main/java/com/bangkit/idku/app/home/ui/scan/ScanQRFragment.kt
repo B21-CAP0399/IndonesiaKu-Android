191c12239aa5c663dd1ac6992e4122509d167304
Leonards03 <edwin.leonards03@gmail.com>
2021-05-23 23:13:22 +0700

    Refactor DashboardFragment to ScanQRFragment

---
+++ app/src/main/java/com/bangkit/idku/app/home/ui/scan/ScanQRFragment.kt
@@ -0,0 +1,68 @@
+package com.bangkit.idku.app.home.ui.scan
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.bangkit.idku.app.databinding.FragmentScanQrBinding
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+
+class ScanQRFragment : Fragment() {
+
+//    private val dashboardViewModel: DashboardViewModel
+    private var binding: FragmentScanQrBinding? = null
+    private lateinit var codeScanner: CodeScanner
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentScanQrBinding.inflate(inflater, container, false)
+//        val root: View = binding.root
+//
+//        val textView: TextView = binding.textDashboard
+//        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            val scannerView = scannerView
+            val activity = requireActivity()
+            codeScanner = CodeScanner(activity, scannerView)
+            codeScanner.decodeCallback = DecodeCallback {
+                activity.runOnUiThread {
+                    Toast.makeText(activity, it.text, Toast.LENGTH_LONG).show()
+                }
+            }
+            scannerView.setOnClickListener {
+                codeScanner.startPreview()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
