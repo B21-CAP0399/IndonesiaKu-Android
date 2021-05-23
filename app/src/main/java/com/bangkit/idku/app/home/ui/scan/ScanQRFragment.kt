0d8bf34216fe9244d43f4107bc7c4a337f3af650
Leonards03 <edwin.leonards03@gmail.com>
2021-05-24 02:00:36 +0700

    Update ScanQRFragment

--- app/src/main/java/com/bangkit/idku/app/home/ui/scan/ScanQRFragment.kt
+++ app/src/main/java/com/bangkit/idku/app/home/ui/scan/ScanQRFragment.kt
@@ -1,19 +1,21 @@
 package com.bangkit.idku.app.home.ui.scan
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.bangkit.idku.app.databinding.FragmentScanQrBinding
-import com.budiyev.android.codescanner.CodeScanner
-import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.*
+import com.budiyev.android.codescanner.CodeScanner.ALL_FORMATS
 
 class ScanQRFragment : Fragment() {
 
-//    private val dashboardViewModel: DashboardViewModel
+    //    private val dashboardViewModel: DashboardViewModel
     private var binding: FragmentScanQrBinding? = null
     private lateinit var codeScanner: CodeScanner
 
@@ -36,19 +38,52 @@ class ScanQRFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        requestCameraPermission()
+        qrCodeScanner()
+    }
+
+    private fun qrCodeScanner() {
         binding?.apply {
             val scannerView = scannerView
             val activity = requireActivity()
             codeScanner = CodeScanner(activity, scannerView)
-            codeScanner.decodeCallback = DecodeCallback {
-                activity.runOnUiThread {
-                    Toast.makeText(activity, it.text, Toast.LENGTH_LONG).show()
+
+            codeScanner.apply {
+                camera = CodeScanner.CAMERA_BACK
+                formats = ALL_FORMATS
+                autoFocusMode = AutoFocusMode.SAFE
+                scanMode = ScanMode.CONTINUOUS
+                isAutoFocusEnabled = true
+                decodeCallback = DecodeCallback {
+                    activity.runOnUiThread {
+                        Toast.makeText(activity, it.text, Toast.LENGTH_LONG).show()
+                    }
+                }
+                errorCallback = ErrorCallback {
+                    activity.runOnUiThread {
+                        Toast.makeText(activity, it.message, Toast.LENGTH_LONG).show()
+                    }
                 }
             }
             scannerView.setOnClickListener {
                 codeScanner.startPreview()
             }
         }
+    }
+
+    private fun requestCameraPermission() {
+        val permission =
+            ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(
+            requireActivity(), arrayOf(android.Manifest.permission.CAMERA), CAMERA_REQUEST_CODE
+        )
     }
 
     override fun onResume() {
@@ -64,5 +99,26 @@ class ScanQRFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(activity, "Permission Denied", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(activity, "Permission Allowed", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+//        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    companion object {
+        private const val CAMERA_REQUEST_CODE = 100
     }
 }
