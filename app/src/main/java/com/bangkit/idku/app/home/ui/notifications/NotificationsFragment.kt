0af7348f57db31321023c274d5f010e8df2cb1cb
Leonards03 <edwin.leonards03@gmail.com>
2021-05-23 23:14:05 +0700

    Code Optimization

--- app/src/main/java/com/bangkit/idku/app/home/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/bangkit/idku/app/home/ui/notifications/NotificationsFragment.kt
@@ -4,42 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.bangkit.idku.app.home.R
-import com.bangkit.idku.app.home.databinding.FragmentNotificationsBinding
+import com.bangkit.idku.app.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    //    private lateinit var notificationsViewModel: NotificationsViewModel
+    private var binding: FragmentNotificationsBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        binding = null
     }
 }
