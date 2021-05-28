c5c746df613761a634260dfc43b19fc5d4754031
Leonards03 <edwin.leonards03@gmail.com>
2021-05-28 13:30:03 +0700

    Login Requirements

---
+++ core/src/main/java/com/bangkit/idku/core/data/model/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.bangkit.idku.core.data.model
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
