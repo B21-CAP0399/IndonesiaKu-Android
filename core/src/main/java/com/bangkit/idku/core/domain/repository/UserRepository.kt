770b9efaffecf56f6f57fd92ff56190b81775df9
Leonards03 <edwin.leonards03@gmail.com>
2021-05-28 13:29:47 +0700

    Add User DataSource & Repository

--- core/src/main/java/com/bangkit/idku/core/domain/repository/UserRepository.kt
+++ core/src/main/java/com/bangkit/idku/core/domain/repository/UserRepository.kt
@@ -1,8 +1,10 @@
 package com.bangkit.idku.core.domain.repository
 
+import com.bangkit.idku.core.data.model.LoggedInUser
+
 interface UserRepository {
-    fun signin()
-    fun login()
-    fun setLoggedInUser()
+    fun signin(username: String, name: String, password: String, confirmPassword: String)
+    fun login(username: String, password: String)
+    fun setLoggedInUser(loggedInUser: LoggedInUser)
     fun logout()
 }
