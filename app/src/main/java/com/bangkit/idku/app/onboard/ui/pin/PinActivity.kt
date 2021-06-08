45d4b6c203aaa355e28d0c56fa0829630282c11e
Leonards03 <edwin.leonards03@gmail.com>
2021-06-08 19:18:02 +0700

    Push

--- app/src/main/java/com/bangkit/idku/app/onboard/ui/pin/PinActivity.kt
+++ app/src/main/java/com/bangkit/idku/app/onboard/ui/pin/PinActivity.kt
@@ -1,8 +1,8 @@
 package com.bangkit.idku.app.onboard.ui.pin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.bangkit.idku.app.R
@@ -10,31 +10,34 @@ import com.bangkit.idku.app.databinding.ActivityPinBinding
 
 class PinActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var binding : ActivityPinBinding? = null
-    private lateinit var viewModel : PinViewModel
-    private lateinit var txtPin : String
+    private var binding: ActivityPinBinding? = null
+    private lateinit var viewModel: PinViewModel
+    private lateinit var txtPin: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPinBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        viewModel = ViewModelProvider(this,ViewModelProvider.NewInstanceFactory())[PinViewModel::class.java]
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        )[PinViewModel::class.java]
 
         binding?.btnEnter?.setOnClickListener(this)
         binding?.btnForgetPin?.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        when(v?.id){
+        when (v?.id) {
             R.id.btn_enter -> {
                 txtPin = binding?.edtPin?.text.toString()
-                if (pinValidation(txtPin)){
+                if (pinValidation(txtPin)) {
                     txtPin = binding?.edtPin?.text.toString()
                     val action = PinActivityDirections.actionPinActivityToNavigationHome()
                     v?.findNavController().navigate(action)
                     binding = null
-                }else{
+                } else {
                     binding?.txtWrongPin?.text = "Your PIN is Wrong"
                     binding?.edtPin?.text?.clear()
                 }
@@ -46,7 +49,7 @@ class PinActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun pinValidation(pin : String) : Boolean{
+    private fun pinValidation(pin: String): Boolean {
         val numberPin = Integer.parseInt(pin)
         return viewModel.getValidationPin(numberPin)
     }
